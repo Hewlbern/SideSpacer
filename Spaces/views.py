3e34ee615a4c069c2c7fcf6a497d9cfcedb2df1c
Michael Holborn <michael.t.holborn@gmail.com>
2018-11-22 00:58:00 +0000

    fixed a bunch of issues

--- Spaces/views.py
+++ Spaces/views.py
@@ -18,11 +18,22 @@ def index(request):
 
     # Render the response and send it back!
 
-    return render(request, 'Spaces/space.html', context_dict)
+    return render(request, 'Spaces/index.html', context_dict)
 
 
 def space(request):
-    context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the databse for a list of ALL categories currently stored.
+    # Order the categories by no likes in descending order .
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in context_dict dictionary
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    # Render the response and send it back!
+
     return render(request, 'Spaces/space.html', context=context_dict)
 
 def show_category(request, category_name_slug):
