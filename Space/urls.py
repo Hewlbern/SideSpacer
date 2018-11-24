1508d3633914848cffe9cd0175bb0c0a4ca9beb8
Michael Holborn <holbyone@gmail.com>
2018-11-24 23:28:55 +1100

    fixing static files

--- Space/urls.py
+++ Space/urls.py
@@ -31,5 +31,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     path(r'^users/', include('users.urls')),
     path(r'^users/', include('django.contrib.auth.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
